5aecf7e30879e71baaaae076fe658d6921a3e5c1
Michael Lin <miclin@berkeley.edu>
2020-02-12 14:31:45 -0800

    Initial Commit

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Michael Lin on 2020/2/12.
+//  Copyright © 2020 Michael Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
