2c2cf6c34bb388d09c91561aad21b345d0e57634
Michael Lin <miclin@berkeley.edu>
2020-02-16 00:46:54 -0800

    Done with detail view

--- Pokedex/Cotrollers/DetailViewController.swift
+++ Pokedex/Cotrollers/DetailViewController.swift
@@ -14,6 +14,13 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var pokemonID: UILabel!
     @IBOutlet weak var pokemonImage: UIImageView!
     @IBOutlet weak var typeImageStackView: UIStackView!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var hpLabel: UILabel!
+    @IBOutlet weak var atkLabel: UILabel!
+    @IBOutlet weak var spAtkLabel: UILabel!
+    @IBOutlet weak var speedLabel: UILabel!
+    @IBOutlet weak var defLabel: UILabel!
+    @IBOutlet weak var spDefLabel: UILabel!
     
     var showPokemon: Pokemon?
     
@@ -24,6 +31,14 @@ class DetailViewController: UIViewController {
             self.pokemonName.text = target.name
             self.pokemonID.text = "#" + String(target.id)
             self.pokemonImage.kf.setImage(with: URL(string: target.imageUrlLarge))
+            self.totalLabel.text = String(target.total)
+            self.hpLabel.text = String(target.health)
+            self.atkLabel.text = String(target.attack)
+            self.spAtkLabel.text = String(target.specialAttack)
+            self.spDefLabel.text = String(target.specialDefense)
+            self.speedLabel.text = String(target.speed)
+            self.defLabel.text = String(target.defense)
+            self.spDefLabel.text = String(target.specialDefense)
             
             typeImageStackView.distribution = .fillEqually
             for index in 0 ..< target.types.count {
